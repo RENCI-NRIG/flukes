045e51e52ff70299f36700de10133fa4723c1d46
Ilya Baldin <ibaldin@users.noreply.github.com>
2011-10-24 19:21:17 +0000

    Minor refactoring.

--- src/main/java/orca/flukes/GUIRequestState.java
+++ src/main/java/orca/flukes/GUIRequestState.java
@@ -23,6 +23,8 @@
 
 package orca.flukes;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -37,7 +39,14 @@ import java.util.Set;
 
 import orca.flukes.ndl.RequestSaver;
 import orca.flukes.ui.ChooserWithNewDialog;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.util.Pair;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.EditingModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.picking.PickedState;
 
 /**
  * Singleton class that holds shared GUI request state. Since dialogs are all modal, no need for locking for now.
@@ -330,4 +339,60 @@ public class GUIRequestState extends GUICommonState implements IDeleteEdgeCallBa
 	public ActionListener getActionListener() {
 		return al;
 	}
+	
+	/**
+	 * Initialize request pane 
+	 */
+	@Override
+	public void addPane(Container c) {
+
+		// Layout<V, E>, VisualizationViewer<V,E>
+		//	        Map<OrcaNode,Point2D> vertexLocations = new HashMap<OrcaNode, Point2D>();
+		
+		Layout<OrcaNode, OrcaLink> layout = new FRLayout<OrcaNode, OrcaLink>(g);
+		
+		//layout.setSize(new Dimension(1000,800));
+		vv = 
+			new VisualizationViewer<OrcaNode,OrcaLink>(layout);
+		// Show vertex and edge labels
+		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<OrcaNode>());
+		vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<OrcaLink>());
+		
+		// Create a graph mouse and add it to the visualization viewer
+		OrcaNode.OrcaNodeFactory onf = new OrcaNode.OrcaNodeFactory(nodeCreator);
+		OrcaLink.OrcaLinkFactory olf = new OrcaLink.OrcaLinkFactory(linkCreator);
+		gm = new EditingModalGraphMouse<OrcaNode, OrcaLink>(vv.getRenderContext(), 
+				onf, olf);
+		
+		// add the plugin
+		PopupVertexEdgeMenuMousePlugin<OrcaNode, OrcaLink> myPlugin = new PopupVertexEdgeMenuMousePlugin<OrcaNode, OrcaLink>();
+		
+		// Add some popup menus for the edges and vertices to our mouse plugin.
+		myPlugin.setEdgePopup(new MouseMenus.RequestEdgeMenu());
+		myPlugin.setVertexPopup(new MouseMenus.RequestNodeMenu());
+		myPlugin.setModePopup(new MouseMenus.ModeMenu());
+		gm.remove(gm.getPopupEditingPlugin());  // Removes the existing popup editing plugin
+		gm.add(myPlugin);
+
+		// Add icon and shape (so pickable areal roughly matches the icon) transformer
+		OrcaNode.OrcaNodeIconShapeTransformer st = new OrcaNode.OrcaNodeIconShapeTransformer();
+		vv.getRenderContext().setVertexShapeTransformer(st);
+		
+		OrcaNode.OrcaNodeIconTransformer it = new OrcaNode.OrcaNodeIconTransformer();
+		vv.getRenderContext().setVertexIconTransformer(it);
+		
+		// add listener to add/remove checkmarks on selected nodes
+		PickedState<OrcaNode> ps = vv.getPickedVertexState();
+        ps.addItemListener(new OrcaNode.PickWithIconListener(it));
+		
+		vv.setGraphMouse(gm);
+
+		vv.setLayout(new BorderLayout(0,0));
+		
+		c.add(vv);
+
+		gm.setMode(ModalGraphMouse.Mode.EDITING); // Start off in editing mode  
+	}
+	
+	
 }
--- src/main/java/orca/flukes/GUIResourceState.java
+++ src/main/java/orca/flukes/GUIResourceState.java
@@ -1,9 +1,16 @@
 package orca.flukes;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
+
+import javax.swing.ImageIcon;
 
 import orca.flukes.ndl.ResourceQueryProcessor;
 
@@ -12,6 +19,16 @@ import org.apache.commons.collections15.Transformer;
 import com.hyperrealm.kiwi.ui.dialog.ExceptionDialog;
 import com.hyperrealm.kiwi.ui.dialog.ProgressDialog;
 import com.hyperrealm.kiwi.util.Task;
+
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.algorithms.layout.StaticLayout;
+import edu.uci.ics.jung.visualization.Layer;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.EditingModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.picking.PickedState;
+import edu.uci.ics.jung.visualization.transform.MutableTransformer;
 
 public class GUIResourceState extends GUICommonState {
 	
@@ -140,4 +157,91 @@ public class GUIResourceState extends GUICommonState {
 		g.addVertex(newOrs);
 		return newOrs;
 	}
+
+	@Override
+	public void addPane(Container c) {
+
+		//Layout<OrcaNode, OrcaLink> layout = new StaticLayout<OrcaNode, OrcaLink>(g);
+		Layout<OrcaNode,OrcaLink> layout = 
+			new StaticLayout<OrcaNode,OrcaLink>(g,
+				new GUIResourceState.LatLonPixelTransformer(new Dimension(5400,2700)));
+
+		//layout.setSize(new Dimension(1000,800));
+		vv = 
+			new VisualizationViewer<OrcaNode,OrcaLink>(layout);
+		// Show vertex and edge labels
+		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<OrcaNode>());
+		vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<OrcaLink>());
+		
+		// Create a graph mouse and add it to the visualization viewer
+		OrcaNode.OrcaNodeFactory onf = new OrcaNode.OrcaNodeFactory(nodeCreator);
+		OrcaLink.OrcaLinkFactory olf = new OrcaLink.OrcaLinkFactory(linkCreator);
+		gm = new EditingModalGraphMouse<OrcaNode, OrcaLink>(vv.getRenderContext(), 
+				onf, olf);
+		
+		// add the plugin
+		PopupVertexEdgeMenuMousePlugin<OrcaNode, OrcaLink> myPlugin = new PopupVertexEdgeMenuMousePlugin<OrcaNode, OrcaLink>();
+		
+		// Add some popup menus for the edges and vertices to our mouse plugin.
+		// mode menu is not set for manifests
+		//myPlugin.setEdgePopup(new MouseMenus.ManifestEdgeMenu());
+		myPlugin.setVertexPopup(new MouseMenus.ResourceNodeMenu());
+		
+		// add map pre-renderer
+		ImageIcon mapIcon = null;
+        String imageLocation = GUIResourceState.WORLD_ICON;
+        try {
+            mapIcon = 
+                    new ImageIcon(GUIResourceState.class.getResource(imageLocation));
+        } catch(Exception ex) {
+            System.err.println("Can't load \""+imageLocation+"\"");
+        }
+        final ImageIcon icon = mapIcon;
+
+        vv.addPreRenderPaintable(new VisualizationViewer.Paintable(){
+            public void paint(Graphics g) {
+                    Graphics2D g2d = (Graphics2D)g;
+                    AffineTransform oldXform = g2d.getTransform();
+                AffineTransform lat = 
+                    vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT).getTransform();
+                AffineTransform vat = 
+                    vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).getTransform();
+                AffineTransform at = new AffineTransform();
+                at.concatenate(g2d.getTransform());
+                at.concatenate(vat);
+                at.concatenate(lat);
+                g2d.setTransform(at);
+                g.drawImage(icon.getImage(), 0, 0,
+                            icon.getIconWidth(),icon.getIconHeight(), vv);
+                g2d.setTransform(oldXform);
+            }
+            public boolean useTransform() { return false; }
+        });
+		
+		gm.remove(gm.getPopupEditingPlugin());  // Removes the existing popup editing plugin
+		gm.add(myPlugin);
+
+		// Add icon and shape (so pickable area roughly matches the icon) transformer
+		OrcaNode.OrcaNodeIconShapeTransformer st = new OrcaNode.OrcaNodeIconShapeTransformer();
+		vv.getRenderContext().setVertexShapeTransformer(st);
+		
+		OrcaNode.OrcaNodeIconTransformer it = new OrcaNode.OrcaNodeIconTransformer();
+		vv.getRenderContext().setVertexIconTransformer(it);
+		
+		// add listener to add/remove checkmarks on selected nodes
+		PickedState<OrcaNode> ps = vv.getPickedVertexState();
+        ps.addItemListener(new OrcaNode.PickWithIconListener(it));
+		
+		vv.setGraphMouse(gm);
+
+		vv.setLayout(new BorderLayout(0,0));
+		
+		c.add(vv);
+
+		gm.setMode(ModalGraphMouse.Mode.TRANSFORMING); // Start off in panning mode  
+		
+		// center over US
+		vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).setTranslate(-700, -500);
+	}
+	
 }
