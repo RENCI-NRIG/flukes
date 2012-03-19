ee13d059f05f0157ddef7ef7888c609ae16e5a76
ibaldin <ibaldin@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2012-03-19 17:51:16 +0000

    removing unnecessary import

--- orca-external/flukes/trunk/src/main/java/orca/flukes/ndl/RequestLoader.java
+++ orca-external/flukes/trunk/src/main/java/orca/flukes/ndl/RequestLoader.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
