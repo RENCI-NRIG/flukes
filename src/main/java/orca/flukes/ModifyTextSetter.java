fc4e5da01178e80a76b2f6f73e9e2326ec6a977f
Ilya Baldin <ibaldin@users.noreply.github.com>
2012-10-31 20:24:07 +0000

    Added modifySlice call from manifest view

---
+++ src/main/java/orca/flukes/ModifyTextSetter.java
@@ -0,0 +1,23 @@
+package orca.flukes;
+
+import orca.flukes.ui.TextAreaDialog.ITextSetter;
+
+/**
+ * Most likely a temporary class that submits modify
+ * request once the text is set
+ * @author ibaldin
+ *
+ */
+public class ModifyTextSetter implements ITextSetter {
+	String sid = null;
+	
+	public ModifyTextSetter(String sliceId) {
+		sid = sliceId;
+	}
+	
+	@Override
+	public void setText(String t) {
+		GUIManifestState.getInstance().modifySlice(sid, t);
+	}
+
+}
