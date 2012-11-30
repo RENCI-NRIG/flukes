ef476b5bf8b80c85e2342eab8057ee64355b529e
Ilya Baldin <ibaldin@users.noreply.github.com>
2012-11-30 20:23:36 +0000

    Minor image update

--- src/main/java/orca/flukes/GUI.java
+++ src/main/java/orca/flukes/GUI.java
@@ -1028,11 +1028,11 @@ public class GUI implements ComponentListener {
 		ORCA_XMLRPC_CONTROLLER("orca.xmlrpc.url", "https://some.hostname.org:11443/orca/xmlrpc", 
 			"Comma-separated list of URLs of the ORCA XMLRPC controllers where you can submit slice requests"),
 		ENABLE_MODIFY("enable.modify", "false", "Enable experimental support for slice modify operations (at your own risk!)"),
-		IMAGE_NAME("image.name", "Debian-6-Standard-Multi-Size-Image-v.1.0.5", 
+		IMAGE_NAME("image.name", "Debian-6-Standard-Multi-Size-Image-v.1.0.6", 
 			"Name of a known image, you can add more images by adding image1.name, image2.name etc. To see defined images click on 'Client Images' button."),
-		IMAGE_URL("image.url", "http://geni-images.renci.org/images/standard/debian/deb6-neuca-v1.0.5.xml", 
+		IMAGE_URL("image.url", "http://geni-images.renci.org/images/standard/debian/deb6-neuca-v1.0.6.xml", 
 			"URL of a known image description file, you can add more images by adding image1.url, image2.url etc."),
-		IMAGE_HASH("image.hash", "104ea3d824906f0e13cebb89b14df232290553b1", 
+		IMAGE_HASH("image.hash", "64ad567ce3b1c0dbaa15bad673bbf556a9593e1c", 
 			"SHA-1 hash of the image description file, you can add more images by adding image1.hash, image2.hash etc.");
 		
 		private final String propName;
